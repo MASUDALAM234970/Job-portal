f4afaa5d932f47c94025904154fa862d5b3cabd6
mdmas <mdmasudalam2349@gmail.com>
2025-01-15 23:55:09 +0600

    Add company management functionality with registration, retrieval, and update endpoints

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRouter from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 
 // Load environment variables
 dotenv.config();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 // User routes
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/company", companyRoute);
 
 // Server setup
 const PORT = process.env.PORT || 8000;
