31cf0662a2bd6d087f0940fe0d71013988ef0552
mdmas <mdmasudalam2349@gmail.com>
2025-01-28 01:47:11 +0600

    Add jobSlice for managing job-related state in Redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authSlice"; // Make sure the import path is correct
+import authReducer from "./authSlice";
+import jobSlice from "./jobSlice"; // Make sure the import path is correct
 
 const store = configureStore({
   reducer: {
     auth: authReducer, // Correct placement inside reducer object
+    job: jobSlice, // Correct placement inside reducer object
   },
 });
 
