6c912ee9c7f97bc42d15874a260fd97a741cbec4
mdmas <mdmasudalam2349@gmail.com>
2025-01-29 00:53:18 +0600

    Add CompaniesTable and CompanyCreate components; implement table structure for displaying registered companies

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,5 +1,56 @@
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Edit2, MoreHorizontal } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export default function CompaniesTable() {
-  return <div>CompaniesTable</div>;
+  return (
+    <div>
+      <Table>
+        <TableCaption> A list of your recent registered companies</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Logo</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead clasName="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableCell>
+            <Avatar>
+              <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" />
+            </Avatar>
+          </TableCell>
+          <TableCell>Company Name</TableCell>
+          <TableCell>18-07-2025</TableCell>
+          <TableCell className="text-right cursor-pointer">
+            <Popover>
+              <PopoverTrigger>
+                <MoreHorizontal />
+              </PopoverTrigger>
+              <PopoverContent className="w-32">
+                <div
+                  //   onClick={() => navigate(`/admin/companies/${company._id}`)}
+                  className="flex items-center gap-2 w-fit cursor-pointer"
+                >
+                  <Edit2 className="w-4" />
+                  <span>Edit</span>
+                </div>
+              </PopoverContent>
+            </Popover>
+          </TableCell>
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
