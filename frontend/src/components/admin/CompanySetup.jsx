0cbe216d4712cb972483eb5c3b4b842163384321
mdmas <mdmasudalam2349@gmail.com>
2025-01-30 00:31:30 +0600

    Add file upload functionality to company update; implement loading state and fetch single company by ID

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../shared/Navbar";
 import { Button } from "../ui/button";
-import { ArrowLeft } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { ArrowLeft, Loader2 } from "lucide-react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import axios from "axios";
+import { COMPANY_API_END_POINT } from "../../utils/constant";
+import { toast } from "sonner";
+import { useSelector } from "react-redux";
 
 export default function CompanySetup() {
   const navigate = useNavigate();
+  const params = useParams();
+  const [loading, setLoading] = useState(false);
+  const { singleCompany } = useSelector((store) => store.company);
   const [input, setInput] = useState({
     name: "",
     description: "",
@@ -25,8 +32,48 @@ export default function CompanySetup() {
   };
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(input);
+    const formData = new FormData();
+    formData.append("name", input.name);
+    formData.append("description", input.description);
+    formData.append("website", input.website);
+    formData.append("location", input.location);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+
+    try {
+      setLoading(true);
+      const res = await axios.put(
+        `${COMPANY_API_END_POINT}/update/${params.id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/admin/companies");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  useEffect(() => {
+    setInput({
+      name: singleCompany.name || "",
+      description: singleCompany.description || "",
+      website: singleCompany.website || "",
+      location: singleCompany.location || "",
+      file: singleCompany.file || null,
+    });
+  }, [singleCompany]);
   return (
     <div>
       <Navbar />
@@ -91,9 +138,15 @@ export default function CompanySetup() {
             </div>
           </div>
 
-          <Button type="submit" className="w-full mt-8 hover:bg-red-500">
-            Save
-          </Button>
+          {loading ? (
+            <Button className="w-full my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              Update
+            </Button>
+          )}
         </form>
       </div>
     </div>
