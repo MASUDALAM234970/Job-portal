260aec0a1639015ce3ac9ce9a81441e783c46990
mdmas <mdmasudalam2349@gmail.com>
2025-01-29 13:42:04 +0600

    Half Complete

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -1,5 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar } from "../shared/Navbar";
+import { Button } from "../ui/button";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 export default function CompanySetup() {
-  return <div>CompanySetup</div>;
+  const navigate = useNavigate();
+  const [input, setInput] = useState({
+    name: "",
+    description: "",
+    website: "",
+    location: "",
+    file: null,
+  });
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    const file = e.target.files?.[0];
+    setInput({ ...input, file });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-xl mx-auto my-10">
+        <form onSubmit={submitHandler}>
+          <div className="flex items-center gap-5 p-8">
+            <Button
+              onClick={() => navigate("/admin/companies")}
+              variant="outline"
+              className="flex items-center gap-2 text-gray-500 font-semibold"
+            >
+              <ArrowLeft />
+              <span>Back</span>
+            </Button>
+            <h1 className="font-bold text-xl">Company Setup</h1>
+          </div>
+          <div>
+            <div className="grid grid-cols-2 gap-4">
+              <Label>Company Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={input.name}
+                onChange={changeEventHandler}
+              />
+            </div>
+
+            <div>
+              <Label>Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={input.description}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Website</Label>
+              <Input
+                type="text"
+                name="website"
+                value={input.website}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Location</Label>
+              <Input
+                type="text"
+                name="location"
+                value={input.location}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Logo</Label>
+              <Input
+                type="file"
+                accept="image/*"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+
+          <Button type="submit" className="w-full mt-8 hover:bg-red-500">
+            Save
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
